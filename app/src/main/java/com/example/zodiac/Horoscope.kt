074e8506a57e23d26f9939114beae30a112d706b
Andrea Lobato <andreaclobato@gmail.com>
2026-04-13 18:58:10 +0200

    Horoscope

---
+++ app/src/main/java/com/example/zodiac/Horoscope.kt
@@ -0,0 +1,16 @@
+package com.example.zodiac
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Horoscope
+val id: String,
+val name: Int,
+val dates: Int,
+val image: Int
+
+
+class MainActivity : AppCompatActivity ()
+ Horoscope
+val horoscopeList: List<List<Horoscope>> = listOf( {
+ } as List<Horoscope>
